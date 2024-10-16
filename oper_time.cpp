d83c22351e702794f191df7f3fd1596cc91a8d72
516 <516@irsural.ru>
2024-10-16 16:46:49 +0500

    A

--- oper_time.cpp
+++ oper_time.cpp
@@ -1,75 +1,16 @@
 #include "oper_time.h"
 
-uint32_t irs::make_cnt_s(uint32_t a_seconds)
-{
-    return a_seconds * 1000;
-}
-
-irs::timer_t::timer_t()
-{
-}
-
-irs::timer_t::timer_t(uint32_t a_milliseconds)
-{
-    m_delay = a_milliseconds;
-}
-
-bool irs::timer_t::check()
-{
-    if (m_is_started && !m_is_stopped && !m_is_result_checked && current_time() >= m_time_start_point + m_delay) {
-        m_is_result_checked = true;
-        return true;
-    }
-    return false;
-}
-
-bool irs::timer_t::stopped() const
-{
-    if (m_is_stopped) {
-        return true;
-    }
-    return false;
-}
-
-
-void irs::timer_t::start()
-{
-    if (m_delay != 0) {
-        m_time_start_point = current_time() - m_worked_time;
-        m_is_started = true;
-        m_is_stopped = false;
-        m_is_result_checked = false;
-    }
-}
-
-void irs::timer_t::stop()
-{
-    if (!m_is_stopped && m_is_started) {
-        m_worked_time = current_time() - m_time_start_point;
-    }
-    m_is_stopped = true;
-}
-
-void irs::timer_t::set(uint32_t a_milliseconds)
-{
-    m_delay = a_milliseconds;
-}
-
-uint32_t irs::timer_t::current_time() const
-{
-    return static_cast<uint32_t>(duration_cast<milliseconds>(system_clock::now().time_since_epoch()).count());
-}
-
 oper_time_t::oper_time_t(eeprom_safe_map_t<combination_t, uint32_t>* ap_eeprom_safe_map,
   int a_save_time_period_min, const combination_t& a_start_combination, bool a_enable) :
   mp_eeprom_safe_map(ap_eeprom_safe_map),
   m_status(status_t::read_time),
   m_is_enabled(a_enable),
   m_combination(a_start_combination),
-  m_combination_time(0),
+  m_combination_time(),
   m_save_time_period_min(a_save_time_period_min),
-  m_delay_timer(irs::make_cnt_s(m_work_timer_period_m/* * 60*/))
+  m_delay_timer(irs::make_cnt_s(m_work_timer_period_m * 60))
 {
+  m_combination_time = m_default_combination_time;
   set_enabled(m_is_enabled);
 }
 
@@ -100,8 +41,11 @@ void oper_time_t::tick()
     } break;
     case status_t::read_time: {
       if (mp_eeprom_safe_map->ready()) {
-        mp_eeprom_safe_map->get_value(m_combination, m_combination_time);
-        m_status = status_t::wait_action_end;
+        if (mp_eeprom_safe_map->get_value(m_combination, m_combination_time)) {
+          m_status = status_t::wait_action_end;
+        } else {
+          m_combination_time = m_default_combination_time;
+        }
       }
     } break;
     case status_t::write_time: {
--- oper_time.h
+++ oper_time.h
@@ -4,36 +4,7 @@
 #include "eeprom_safe_map.h"
 #include <array>
 #include <sd_page_mem.h>
-#include <chrono>
-
-using namespace std;
-using namespace std::chrono;
-
-namespace irs {
-    uint32_t make_cnt_s(uint32_t a_seconds);
-
-    class timer_t
-    {
-    public:
-        explicit timer_t();
-        explicit timer_t(uint32_t a_milliseconds);
-        [[nodiscard]] bool check();
-        [[nodiscard]] bool stopped() const;
-        void start();
-        void stop();
-        void set(uint32_t a_milliseconds);
-
-    private:
-        uint32_t m_time_start_point = 0;
-        uint32_t m_delay = 0;
-        uint32_t m_worked_time = 0;
-        bool m_is_started = false;
-        bool m_is_stopped = true;
-        bool m_is_result_checked = false;
-
-        [[nodiscard]] uint32_t current_time() const;
-    };
-}
+#include "irs_simulation.h"
 
 using combination_t = std::array<uint8_t, 8>;
 
