d7effa2258c250fe9968c0da86b8b8d3988c1c7f
516 <516@irsural.ru>
2024-11-26 21:02:59 +0500

    Добавлен статус поиска наименьшего времени наработки в oper_time

--- main.cpp
+++ main.cpp
@@ -12,19 +12,18 @@ using namespace std::chrono;
 
 const string path = "/home/u516/tmp/eeprom.txt";
 
-const uint32_t eeprom_size_bytes = 1 << 13;
 ofstream os;
 
 const uint32_t page_size_bytes = 32;
-
-constexpr uint32_t page_count = eeprom_size_bytes / page_size_bytes;
+constexpr uint32_t page_count = 32;
+const uint32_t sector_size_pages = 16;
 
 array<uint8_t, page_size_bytes> page{};
 
 void init_eeprom_txt()
 {
     os.open(path);
-    for (int i = 0; i < eeprom_size_bytes; ++i) {
+    for (int i = 0; i < page_size_bytes * page_count; ++i) {
         os << char(0xff);
     }
     os.close();
@@ -36,7 +35,7 @@ int main() {
 #ifdef INIT
     init_eeprom_txt();
 #endif
-    eeprom_safe_map_t<combination_t, uint32_t> m_eeprom_safe_map(new sd_page_mem_t(page_count, page_size_bytes), 0, eeprom_size_bytes, 8,
+    eeprom_safe_map_t<combination_t, uint32_t> m_eeprom_safe_map(new sd_page_mem_t(page_count, page_size_bytes), 0, page_count, sector_size_pages,
                                                                  { 0, 0, 0, 0, 0, 0, 0, 0 }, { 0x7f, 0x7f, 0x7f, 0x7f, 0x7f, 0x7f, 0x7f, 0x7f });
     oper_time_t m_oper_time(&m_eeprom_safe_map, 1, { 0, 0, 0, 0, 0, 0, 0, 0 }, true);
 //    m_oper_time.reset_all();
@@ -45,18 +44,22 @@ int main() {
     combination_t combination {1, 1, 1, 1, 1, 1, 1, 0};
     bool combination_changed = false;
 
+    uint32_t tmp = 800;
+
+    m_eeprom_safe_map.replace_key({1, 1, 1, 1, 1, 1, 1, 4}, {2, 2, 2, 2, 2, 2, 2, 6}, tmp);
+
     timer.start();
     while (true) {
-        if (combination_changed && m_oper_time.ready()) {
-            m_oper_time.set_combination(combination);
-            combination_changed = false;
-        }
+//        if (combination_changed && m_oper_time.ready()) {
+//            m_oper_time.set_combination(combination);
+//            combination_changed = false;
+//        }
         m_oper_time.tick();
-        if (timer.check()) {
-            combination[7] = (combination[7] + 1) % 2;
-            combination_changed = true;
-            timer.start();
-        }
+//        if (timer.check()) {
+//            combination[7] = (combination[7] + 1) % 5;
+//            combination_changed = true;
+//            timer.start();
+//        }
     }
 
     return 0;
